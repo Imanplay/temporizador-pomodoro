cff50c46899baed2f95661a2d02c718c12cfc4e7
Imanplay <imanolsanxhez0321@gmail.com>
2023-09-19 22:34:39 -0400

    cree la funcion que ejecuta la aplicacion

---
+++ app.js
@@ -0,0 +1,16 @@
+let minutos = 25;
+let segundos = 0;
+
+function contador(){
+   console.log(minutos, segundos);
+     if (segundos === 0 && minutos > 0){
+        minutos = minutos -1;
+        segundos = 59;
+    } else {
+        segundos = segundos -1;
+    }
+
+
+}
+setInterval(()=>contador(), 1000);
+
