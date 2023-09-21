60796809b3c265715e839bf46bc480c73b1ffd36
Imanplay <imanolsanxhez0321@gmail.com>
2023-09-21 19:21:58 -0400

    agregue funcion de descanso

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-let minutos = 25;
-let segundos = 0;
+let minutos = 0;
+let segundos = 5;
 let intervalo;
+let isDescanso = false;
 
 function contador() {
     if (segundos === 0 && minutos > 0) {
@@ -9,11 +10,26 @@ function contador() {
     } else {
         segundos = segundos - 1;
     }
+    if (minutos === 0 && segundos === 0){
+      pausar(); 
+      isDescanso = !isDescanso;
+      minutos = isDescanso ? 5 : 25;
+      segundos = 0; 
+      iniciarContador();
+    }
+
     document.getElementById('input').innerHTML = minutos + ' : ' + segundos;
 }
+
 function iniciarContador() {
     intervalo = setInterval(() => contador(), 1000);
 
+}
+
+new function descansar (){
+    if (minutos && segundos === 0){
+        return (isDescanso);
+    } 
 }
 
 function pausar() {
