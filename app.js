dd5a03de5d167bee751b5d8ca548747f1f6baa6f
Imanplay <145399492+Imanplay@users.noreply.github.com>
2023-09-24 17:58:06 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,23 +1,50 @@
 let minutos = 25;
 let segundos = 0;
-let intervalo  ;
+let intervalo;
+let isDescanso = false;
 
-function contador(){
-     if (segundos === 0 && minutos > 0){
-        minutos = minutos -1;
+function contador() {
+    if (segundos === 0 && minutos > 0) {
+        minutos = minutos - 1;
         segundos = 59;
     } else {
-        segundos = segundos -1;
+        segundos = segundos - 1;
     }
-document.getElementById('input').innerHTML = minutos + ' : ' + segundos;
+    if (minutos === 0 && segundos === 0) {
+        pausar();
+        isDescanso = !isDescanso;
+        minutos = isDescanso ? 5 : 25;
+        segundos = 0;
+        iniciarContador();
+    }
+
+    document.getElementById('input').innerHTML = minutos + ' : ' + segundos;
 }
-function iniciarContador(){
-intervalo = setInterval(()=>contador(), 1000);
+
+function iniciarContador() {
+    intervalo = setInterval(() => contador(), 1000);
 
 }
 
-function pausar(){
+new function descansar() {
+    if (minutos && segundos === 0) {
+        return (isDescanso);
+    }
+}
+
+function pausar() {
     clearInterval(intervalo);
-     intervalo= null;
+    intervalo = null;
 }
 iniciarContador()
+
+let object = {
+//almacenamiento: localStorage.setItem("tiempo", minutos, segundos),
+almacenamiento2:localStorage.setItem("estado", JSON.stringify(Object))
+};
+
+/*function reiniciarContador(){
+    onclick(document.getElementById('boton'))
+
+    iniciarContador()
+}*/
