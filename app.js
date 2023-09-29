687e177c01d2b3e9e977d5c7d04154eb16ab5290
Imanplay <imanolsanxhez0321@gmail.com>
2023-09-28 22:04:21 -0400

    agregue deshabilitacion de botones, junto con variables de cada boton

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
-let minutos = 0;
-let segundos = 5;
+let minutos = 25;
+let segundos = 0;
 let intervalo;
 let isDescanso = false;
-
+const botonStart = document.getElementById('botonStart');
+const botonPausa = document.getElementById('botonStop')
+const span = document.getElementById('input')
 function contador() {
     if (segundos === 0 && minutos > 0) {
         minutos = minutos - 1;
@@ -11,29 +13,33 @@ function contador() {
         segundos = segundos - 1;
     }
     if (minutos === 0 && segundos === 0){
-      pausar(); 
-      isDescanso = !isDescanso;
-      minutos = isDescanso ? 5 : 25;
-      segundos = 0; 
-      iniciarContador();
+        pausar();
+        isDescanso = !isDescanso;
+        minutos = isDescanso ? 5 : 25;
+        segundos = 0;
+        iniciarContador();
     }
-
-    document.getElementById('input').innerHTML = minutos + ' : ' + segundos;
+   
+    document.getElementById('input').innerHTML = minutos.toString().padStart(2, '0') + ' : ' + segundos.toString().padStart(2, );
 }
 
 function iniciarContador() {
     intervalo = setInterval(() => contador(), 1000);
-
+    botonStart.disabled = true;
+    botonPausa.disabled = false;
 }
 
 new function descansar (){
     if (minutos && segundos === 0){
         return (isDescanso);
-    } 
+    }
 }
 
 function pausar() {
     clearInterval(intervalo);
     intervalo = null;
+    botonPausa.disabled = true;
+    botonStart.disabled = false;
+    
 }
-iniciarContador()
+iniciarContador();
